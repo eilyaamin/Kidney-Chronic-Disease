8b80b1f4dc19e60ac4107f2df624cb2092ebbcb9
eilya <iliya.amin2016@gmail.com>
2023-05-25 12:30:59 +0000

    project refactor with sass and components

---
+++ client/src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <div className='body'>Body</div>
+  )
+}
+
+export default Body
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      Chronic Kidney Disease Predictor
+    </div>
+  )
+}
+
+export default Navbar
