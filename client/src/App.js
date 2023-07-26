24c8ffbf87fd7403fa9ea7c2a81d92b143ab963d
EILYAAMIN <iliya.amin2016@gmail.com>
2023-07-26 10:21:52 +0200

    saving work

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import './sass/main.scss'
-import LandingPage from './components/LandingPage'
+import logo from './logo.svg';
+import './App.css';
 
-function App () {
+function App() {
   return (
-    <div className="App" >
-      <LandingPage />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
