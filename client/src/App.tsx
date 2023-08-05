c2335f3d00724b7b1bae1db228f76164a9ac6743
EILYAAMIN <iliya.amin2016@gmail.com>
2023-08-05 16:04:28 +0200

    saving work

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import { Button } from "@nextui-org/button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Press me</Button>
+    </div>
+  );
+}
+
+export default App;
--- server/src/preprocess.py
+++ server/src/preprocess.py
@@ -27,15 +27,15 @@ class Preprocessor:
         self._handle_missing_values()
         self._encode_categorical_columns()
         self._preform_feature_selection()
-        self.dataset.to_csv("NEW_DATASET.csv", index=False)
+        # self.dataset.to_csv("NEW_DATASET.csv", index=False)
 
     def _load_data(self, file_path):
         self.dataset = pd.read_csv(file_path)
 
     def _handle_missing_values(self):
-        # Drop irrelevant columns (if any)
+
         self.dataset = self.dataset.drop("id", axis=1)
-        # Handling missing values in numerical columns
+
         self.numerical_cols = self.dataset.select_dtypes(
             include=["float64", "int64"]
         ).columns
@@ -46,7 +46,6 @@ class Preprocessor:
             self.dataset[self.numerical_cols]
         ).round(3)
 
-        # Handling missing values in categorical columns
         self.categorical_cols = self.dataset.select_dtypes(include=["object"]).columns
         self.dataset[self.categorical_cols] = self.dataset[
             self.categorical_cols
@@ -57,43 +56,34 @@ class Preprocessor:
             include=["int64", "float64"]
         ).columns
 
-        # Use MinMaxScaler to normalize numerical columns
         scaler = MinMaxScaler()
         self.dataset[numerical_columns] = scaler.fit_transform(
             self.dataset[numerical_columns]
         )
 
     def _encode_categorical_columns(self):
-        # Get categorical columns
+
         categorical_columns = self.dataset.select_dtypes(include=["object", "category"])
-        # Encode label column
+
         le = LabelEncoder()
         label_mapping = {"ckd": 1, "notckd": 0}
         le.classes_ = pd.Series(list(label_mapping.keys()))
         self.dataset["classification"] = self.dataset["classification"].str.strip()
         self.dataset["classification"] = le.transform(self.dataset["classification"])
 
-        # Encode categorical columns
         for column in categorical_columns:
             label_encoder = LabelEncoder()
             self.dataset[column] = label_encoder.fit_transform(self.dataset[column])
 
     def _preform_feature_selection(self):
-        # Separate the features (X) and the target (y) from the dataset
         X = self.dataset.drop("classification", axis=1)
         y = self.dataset["classification"]
-
-        # Perform feature selection with SelectKBest
         selector = SelectKBest(f_classif, k=14)
         X_new = selector.fit_transform(X, y)
 
-        # Get the indices of the selected features
         selected_feature_indices = selector.get_support(indices=True)
-
-        # Get the names of the selected features
         selected_feature_names = X.columns[selected_feature_indices]
 
-        # Update self.dataset with the selected features
         self.dataset = pd.concat(
             [pd.DataFrame(X_new, columns=selected_feature_names), y], axis=1
         )
